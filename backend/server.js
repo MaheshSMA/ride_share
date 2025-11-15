f2882424df3dad5ea5cc754c4fb02060191fd063
Anonymous-2004 <maheshmalge004@gmail.com>
2025-11-15 13:51:04 +0530

    inital setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load env variables
+dotenv.config();
+
+// Connect Mongo
+connectDB();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
